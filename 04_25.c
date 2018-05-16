4b90ee0f357445d9ee0c70ba2d3896f0ede09dd0
FlorentE96 <flo.ecochard@gmail.com>
2018-05-16 14:31:00 +0900

    Commented the code

--- 04_25.c
+++ 04_25.c
@@ -17,6 +17,13 @@ int gcd(int A, int B);
 void flip(int * A, int * B);
 static inline char isMultiple(int A, int B) {return (A%B)==0;} // Returns TRUE if A is a multiple of B, FALSE otherwise
 
+
+/**
+ * Main function, the exercise number is passed as a command line argument
+ * @param argc : the number of input arguments passed via the command line
+ * @param argv : an array of string corresponding to the command line arguments
+ * @return -1 for error, 0 for success
+ **/
 int main(int argc, char *argv[]) {
   if(argc > 1) {
     if(!strcmp(argv[1], "4.1"))
@@ -44,8 +51,14 @@ int main(int argc, char *argv[]) {
     printf("Not enough input arguments.\n\rUsage: myProgram.exe QUESTION_NUMBER\n\r");
     return -1;
   }
+  return 0;
 }
 
+/**
+ * Exercise 4.1
+ * Displays the factorial of an inputted number.
+ * @see int#facto(int)
+ **/
 void q_4_1(void) {
   printf("\n\r### Exercise 4.1 : factorial ###\r\n");
   printf("Please enter n\n\r");
@@ -54,9 +67,13 @@ void q_4_1(void) {
   if(ret == 0) // The format d
     printf("Please type an integer\n\r");
   else
-    printf("Result : %d\n\r", facto(n));  
+    printf("Result : %d\n\r", facto(n));
 }
 
+/**
+ * Exercise 4.2
+ * Displays the number of combinations possible with an user-inputted size of set and subset.
+ **/
 void q_4_2(void) {
   printf("\n\r### Exercise 4.2 : combinations ###\r\n");
   printf("Please enter the set size\n\r");
@@ -79,6 +96,11 @@ void q_4_2(void) {
   printf("Result = %d\n\r", facto(set)/(facto(subset)*facto(set-subset)));
 }
 
+/**
+ * Exercise 4.3
+ * Displays the GCD of 2 inputted numbers.
+ * @see int#gcd(int,int)
+ **/
 void q_4_3(void) {
   printf("\n\r### Exercise 4.3 : GCD ###\r\n");
   printf("Please enter the first number (A)\n\r");
@@ -93,10 +115,16 @@ void q_4_3(void) {
   if(ret == 0) {
     printf("Please type an integer\n\r");
     return;
-  }  
-  printf("Result : %d\n\r", gcd(A, B));  
+  }
+  printf("Result : %d\n\r", gcd(A, B));
 }
 
+/**
+ * Exercise 4.4
+ * Displays all the multiples of ONLY 5, 9 or 11.
+ * Stops when the current number is a multiple of 5, 9 and 11.
+ * @see int#isMultiple(int,int)
+ **/
 void q_4_4(void) {
   printf("\n\r### Exercise 4.4 ###\r\n");
   int num = 0;
@@ -120,6 +148,10 @@ void q_4_4(void) {
   printf("\n\r");
 }
 
+/**
+ * Exercise 4.5
+ * Displays the time schedule of a bus between 10am and 4pm
+ **/
 void q_4_5(void) {
   printf("\n\r### Exercise 4.5 ###\r\n");
   const int starting_time = 7*60;
@@ -132,6 +164,10 @@ void q_4_5(void) {
   }
 }
 
+/**
+ * Exercise 4.6
+ * Displays the maximum number of layers that can be made with a certain number of bricks.
+ **/
 void q_4_6(void) {
   printf("\n\r### Exercise 4.6 ###\r\n");
   printf("Please enter the number of bricks :\t");
@@ -149,6 +185,10 @@ void q_4_6(void) {
   printf("You can make %d layers.\n\r", layers);
 }
 
+/**
+ * Exercise 4.7
+ * Displays how long it will take for a bank account to reach 10000000 yen
+ **/
 void q_4_7(void) {
   printf("\n\r### Exercise 4.7 ###\r\n");
   int months = 0;
@@ -157,9 +197,14 @@ void q_4_7(void) {
     months++;
     balance = (balance*1.001) + 10000;
   } while(balance < 1000000);
-  printf("It will take %d years and %d months\r\n", months/12, months%12);    
+  printf("It will take %d years and %d months\r\n", months/12, months%12);
 }
 
+/**
+ * Exercise 5.6
+ * Sorts students' average grade from smallest to biggest
+ * @see void#flip(int*,int*)
+ **/
 void q_5_6(void) {
   printf("\n\r### Exercise 5.6 ###\r\n");
   printf("Please enter each student's grade:\n\r");
@@ -189,6 +234,10 @@ void q_5_6(void) {
   }
 }
 
+/**
+ * Exercise 5.7
+ * Displays the histogram of inputted students grades
+ **/
 void q_5_7(void) {
   printf("\n\r### Exercise 5.7 ###\r\n");
    printf("Please enter each student's grade:\n\r");
@@ -215,6 +264,11 @@ void q_5_7(void) {
   }
 }
 
+/**
+ * Computes the factorial of 2 number.
+ * @parameter n : the integer of which the factorial must be determined
+ * @returns : n!
+ **/
 int facto(int n) {
   int result = 1;
   for(int i=1;i<=n;i++)
@@ -222,6 +276,12 @@ int facto(int n) {
   return result;
 }
 
+/**
+ * Computes de GCD of 2 numbers using successive substractions algorithm
+ * @parameter A : The first number
+ * @parameter B : the second number
+ * @return an int corresponding to the GCD
+ **/
 int gcd(int A, int B) {
   int diff = A;
   if (A == B)
@@ -239,6 +299,11 @@ int gcd(int A, int B) {
   return A;
 }
 
+/**
+ * Flips 2 ints in place.
+ * @parameter A : a pointer to the first int
+ * @parameter B : a pointer to the second int
+ **/
 void flip(int * A, int * B) {
   int temp;
   temp = *A;
